7bb2d9a45bee3a5fce53c04382b25debe99a5bbe
Arminas <arminaszinkevicius@gmail.com>
2023-02-11 13:39:23 +0200

    button doesnt work to show form

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @toggle-add-task="$emit('toggle-add-task')" text="Add task" color="grey"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button
+    }
+
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
